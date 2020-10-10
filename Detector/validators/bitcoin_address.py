c953bee9a8f109436d07817d5eb3b7d04eccb12a
Martin Šmíd <martin.smid94@seznam.cz>
2020-10-10 22:21:32 +0200

    Update bitcoin address validator

--- Detector/validators/bitcoin_address.py
+++ Detector/validators/bitcoin_address.py
@@ -2,19 +2,29 @@ from hashlib import sha256
 from typing import Any
 
 from Detector.codecs.base58 import Base58
+from Detector.codecs.segwit import Segwit
 
 from .validator import Validator
 
 
 class BitcoinAddress(Validator):
-    decoder = Base58()
+    base58 = Base58()
+    segwit = Segwit()
 
     def is_valid(self, data: Any) -> bool:
         """
         https://rosettacode.org/wiki/Bitcoin/address_validation#Python
         """
         try:
-            bcbytes = self.decoder.decode(data)
-            return bcbytes[-4:] == sha256(sha256(bcbytes[:-4]).digest()).digest()[:4]
+            if data[0] in ["1", "3"]:
+                bc_bytes = self.base58.decode(data)
+                return (
+                    bc_bytes[-4:] == sha256(sha256(bc_bytes[:-4]).digest()).digest()[:4]
+                )
+
+            hrp, decoded = self.segwit.decode(data, hrp="bc")
+            if hrp is None:
+                hrp, decoded = self.segwit.decode(data, hrp="tb")
+            return hrp is not None and decoded is not None
         except Exception:
             return False

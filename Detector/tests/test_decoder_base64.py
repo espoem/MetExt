13fd0e4cab0db76aca2a0072b4a6ce20c2f1b7b1
Martin Šmíd <martin.smid94@seznam.cz>
2020-10-10 13:17:03 +0200

    Change tests to functions

--- Detector/tests/test_decoder_base64.py
+++ Detector/tests/test_decoder_base64.py
@@ -3,9 +3,8 @@ import pytest
 from Detector.codecs.base64 import Base64
 
 
-class TestBase64Decoder:
-    @pytest.mark.parametrize(
-        "encoded, expected", [(b"MQ==", b"1"), ("MQ", b"1"), ("YmFzZTY0", b"base64")]
-    )
-    def test_decode(self, encoded, expected):
-        assert Base64().decode(encoded) == expected
+@pytest.mark.parametrize(
+    "encoded, expected", [(b"MQ==", b"1"), ("MQ", b"1"), ("YmFzZTY0", b"base64")]
+)
+def test_decode(encoded, expected):
+    assert Base64().decode(encoded) == expected
--- Detector/tests/test_extractor_bitcoin_address.py
+++ Detector/tests/test_extractor_bitcoin_address.py
@@ -3,40 +3,42 @@ import pytest
 from Detector.extractors.bitcoin_address import BitcoinAddress
 
 
-class TestBitcoinAddressExtractor:
-    @pytest.mark.parametrize(
-        "input_data",
+@pytest.mark.parametrize(
+    "input_data",
+    [
+        "",
+        "1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i",
         [
-            "",
             "1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i",
-            [
-                "1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i",
-                "1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i",
-            ],
-            "1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i\n1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i",
+            "1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i",
         ],
-    )
-    def test_extract_from_returns_empty_collection(self, input_data):
-        extracted = BitcoinAddress().extract_from(input_data)
-        assert len(list(extracted)) == 0
+        "1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i\n1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i",
+        "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j"
+    ],
+)
+def test_extract_from_returns_empty_collection(input_data):
+    extracted = BitcoinAddress().extract_from(input_data)
+    assert len(list(extracted)) == 0
 
-    @pytest.mark.parametrize(
-        "input_data, expected",
-        [
-            (
+
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [
+        (
                 "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j",
                 ["17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j"],
-            ),
-            (
-                "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j",
-                [
-                    "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j",
-                    "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j",
-                ],
-            ),
-        ],
-    )
-    def test_extract_from(self, input_data, expected):
-        extracted = list(BitcoinAddress().extract_from(input_data))
-        assert len(extracted) == len(expected)
-        assert not set(extracted).isdisjoint(expected)
+        ),
+        (
+                "-17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j-17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j-",
+                ["17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j", "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j"],
+        ),
+        (
+                ["-17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j-", "\n3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy\nabctest"],
+                ["17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j", "3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy"],
+        ),
+    ],
+)
+def test_extract_from(input_data, expected):
+    extracted = list(BitcoinAddress().extract_from(input_data))
+    assert len(extracted) == len(expected)
+    assert not set(extracted).isdisjoint(expected)

9b5c9daa7a0bee15b6abcdb17603f7bd3b913ac0
Martin Šmíd <martin.smid94@seznam.cz>
2020-10-10 22:42:38 +0200

    Add segwit address extraction

--- Detector/extractors/bitcoin_address.py
+++ Detector/extractors/bitcoin_address.py
@@ -1,18 +1,20 @@
 import re
 from typing import Iterable, List, Union
 
-from Detector.validators.bitcoin_address import BitcoinAddress as BTC
+from Detector.validators.bitcoin_address import BitcoinAddress as Btc
 
 from .extractor import Extractor
 
-PATTERN_BTC = r"[13][a-km-zA-HJ-NP-Z1-9]{25,34}"
-RE_BTC = re.compile(PATTERN_BTC)
+PATTERN_BTC_BASE58 = r"[13][a-km-zA-HJ-NP-Z1-9]{25,34}"
+PATTERN_BTC_SEGWIT = r"(?:[bB][cC])1[a-zA-HJ-NP-Z0-9]{25,39}"
+RE_BTC = re.compile("{}|{}".format(PATTERN_BTC_SEGWIT, PATTERN_BTC_BASE58))
 
 
 class BitcoinAddress(Extractor):
+    btc = Btc()
+
     def extract_from(self, data: Union[str, List[str]]) -> Iterable[str]:
         btc_addresses = []
         for part in data if isinstance(data, list) else data.splitlines(keepends=False):
             btc_addresses.extend(RE_BTC.findall(part))
-        btc = BTC()
-        yield from (address for address in btc_addresses if btc.is_valid(address))
+        yield from (address for address in btc_addresses if self.btc.is_valid(address))
--- Detector/tests/test_extractor_bitcoin_address.py
+++ Detector/tests/test_extractor_bitcoin_address.py
@@ -14,6 +14,9 @@ from Detector.extractors.bitcoin_address import BitcoinAddress
         ],
         "1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i\n1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i",
         "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j",
+        "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
+        "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3pjxtptv",
+        "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t5",
     ],
 )
 def test_extract_from_returns_empty_collection(input_data):
@@ -43,6 +46,21 @@ def test_extract_from_returns_empty_collection(input_data):
             [
                 "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j",
                 "3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy",
+            ],
+        ),
+        (
+            "bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj",
+            ["bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj"],
+        ),
+        (
+            "BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4",
+            ["BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4"],
+        ),
+        (
+            "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j-bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj",
+            [
+                "17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j",
+                "bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj",
             ],
         ),
     ],

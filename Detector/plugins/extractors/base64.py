11daf1f12c5e1553eea112bcedd01aa486531a2a
Martin Šmíd <martin.smid94@seznam.cz>
2021-01-01 16:50:35 +0100

    Fix base64 extractor

--- Detector/plugins/extractors/base64.py
+++ Detector/plugins/extractors/base64.py
@@ -5,7 +5,7 @@ from Detector.plugin_base import BaseExtractor
 from Detector.plugins.validators.base64 import Base64Validator
 
 # https://www.rise4fun.com/Bek/tutorial/base64
-PATTERN_BASE64 = r"(?:[A-Za-z0-9+/]{4})+(?:[A-Za-z0-9+/][AQgw]==|[A-Za-z0-9+/]{2}[AEIMQUYcgkosw048]=)?"
+PATTERN_BASE64 = r"(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/][AQgw]==|[A-Za-z0-9+/]{2}[AEIMQUYcgkosw048]=)?"
 RE_BASE64 = re.compile(r"\b{}\b".format(PATTERN_BASE64))
 
 
@@ -25,5 +25,7 @@ class Base64Extractor(BaseExtractor):
         """
         for part in _input if isinstance(_input, list) else _input.splitlines():
             yield from (
-                b64 for b64 in RE_BASE64.findall(part) if Base64Validator.run(part)
+                b64
+                for b64 in RE_BASE64.findall(part)
+                if b64 and Base64Validator.run(part)
             )

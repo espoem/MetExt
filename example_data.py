21001ef9f9199fff4d5ae4c5ded2ad9c50aafea4
Martin Šmíd <martin.smid94@seznam.cz>
2021-01-01 11:56:35 +0100

    Add data uris example to dummy data

--- example_data.py
+++ example_data.py
@@ -40,6 +40,12 @@ if __name__ == "__main__":
         "http://seznam.cz",
         "https://devdocs.io/python~3.5/library/stdtypes#str.translate",
     ]
+    data_uris = [
+        "data:text/vnd-example+xyz;foo=bar;base64,R0lGODdh",
+        "data:text/plain;charset=UTF-8;page=21,the%20data:1234,5678",
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==",
+        "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDADIiJSwlHzIsKSw4NTI7S31RS0VFS5ltc1p9tZ++u7Kfr6zI4f/zyNT/16yv+v/9////////wfD/////////////2wBDATU4OEtCS5NRUZP/zq/O////////////////////////////////////////////////////////////////////wAARCAAYAEADAREAAhEBAxEB/8QAGQAAAgMBAAAAAAAAAAAAAAAAAQMAAgQF/8QAJRABAAIBBAEEAgMAAAAAAAAAAQIRAAMSITEEEyJBgTORUWFx/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/AOgM52xQDrjvAV5Xv0vfKUALlTQfeBm0HThMNHXkL0Lw/swN5qgA8yT4MCS1OEOJV8mBz9Z05yfW8iSx7p4j+jA1aD6Wj7ZMzstsfvAas4UyRHvjrAkC9KhpLMClQntlqFc2X1gUj4viwVObKrddH9YDoHvuujAEuNV+bLwFS8XxdSr+Cq3Vf+4F5RgQl6ZR2p1eAzU/HX80YBYyJLCuexwJCO2O1bwCRidAfWBSctswbI12GAJT3yiwFR7+MBjGK2g/WAJR3FdF84E2rK5VR0YH/9k=",
+    ]
 
     result = [
         "".join(random.choice(printable) for _ in range(random.randint(0, 10000)))
@@ -47,7 +53,7 @@ if __name__ == "__main__":
         + item
         + random.choice(string.whitespace)
         + "".join(random.choice(printable) for _ in range(random.randint(0, 30000)))
-        for item in btc_addresses + ips + emails + md5s + urls
+        for item in btc_addresses + ips + emails + md5s + urls + data_uris
     ]
 
     with open("examples/gen/ex_input.txt", "w", encoding="utf-8") as f:

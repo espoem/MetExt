7f4df0ac0fa916e08c674ec980535c738ef447b2
Martin Šmíd <martin.smid94@seznam.cz>
2021-01-19 17:34:41 +0100

    Add litecoin address extractor

---
+++ metext/plugins/extractors/litecoin.py
@@ -0,0 +1,33 @@
+import re
+from typing import Iterable, List, Union
+
+from metext.plugin_base import BaseExtractor
+from metext.plugins.validators.litecoin import LitecoinValidator
+
+PATTERN_LTC_BASE58 = r"\b[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}\b"
+RE_LTC = re.compile(PATTERN_LTC_BASE58)
+
+
+class LitecoinAddress(BaseExtractor):
+    PLUGIN_NAME = "ltc"
+
+    @classmethod
+    def run(cls, _input: Union[str, List[str]], *args, **kwargs) -> Iterable[str]:
+        """Extracts valid Litecoin addresses from a string or a list of strings.
+
+        Looks for addresses that start with 'M', 'L', or '3' char.
+
+        .. warning::
+            An address starting with '3' may represent a Bitcoin address.
+
+        :param _input: String or a list of strings
+        :return: Generator of found valid Litecoin addresses
+        """
+        for part in _input if isinstance(_input, list) else _input.splitlines():
+            if not part:
+                continue
+            yield from (
+                address
+                for address in RE_LTC.findall(part)
+                if LitecoinValidator.run(address)
+            )

cc7af7bbe3c8aaf298d6a56b6938c912c217bb5d
Martin Šmíd <martin.smid94@seznam.cz>
2021-02-23 21:21:24 +0100

    Add z85 decoder

--- metext/plugins/decoders/ascii85.py
+++ metext/plugins/decoders/ascii85.py
@@ -2,6 +2,7 @@ import base64
 from typing import Optional
 
 from metext.plugin_base import BaseDecoder, Decodable
+from metext.utils import z85
 
 
 class Ascii85Decoder(BaseDecoder):
@@ -18,4 +19,21 @@ class Ascii85Decoder(BaseDecoder):
         try:
             return base64.a85decode(_input)
         except Exception:
+            return None
+
+
+class Z85Decoder(BaseDecoder):
+    PLUGIN_NAME = "z85"
+
+    @classmethod
+    def run(cls, _input: Decodable, **kwargs) -> Optional[bytes]:
+        """Decodes Z85 encoded bytes-like object or ASCII `data` string.
+
+        :param _input: Z85 encoded (bytes) string
+        :param kwargs:
+        :return: `None` if `data` couldn't be decoded, else decoded byte string'
+        """
+        try:
+            return z85.decode(_input)
+        except:
             return None

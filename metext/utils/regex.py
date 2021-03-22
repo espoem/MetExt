639fa3c8ad4cd59e1cd5222f7cae690beb5ef5a0
Martin Šmíd <martin.smid94@seznam.cz>
2021-03-22 13:58:19 +0100

    Update btc wif regex

--- metext/utils/regex.py
+++ metext/utils/regex.py
@@ -93,7 +93,7 @@ PATTERN_ADA_SHELLEY = (
 
 RE_BTC = re.compile("{}|{}".format(PATTERN_BTC_SEGWIT, PATTERN_BTC_BASE58))
 RE_BTC_WIF = re.compile(
-    r"\b(?:[59][123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{50}|[LKc][123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{51})\b"
+    r"\b(?:[59][a-km-zA-HJ-NP-Z1-9]{50}|[LKc][a-km-zA-HJ-NP-Z1-9]{51})\b"
 )
 RE_BTC_PRIVKEY = re.compile(r"\b[0-9a-fA-f]{64}\b")
 RE_LTC = re.compile(r"\b[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}\b")

392706b5359649a4b53768d2f93c166027e4bc46
Martin Šmíd <martin.smid94@seznam.cz>
2021-04-10 23:06:48 +0200

    Refactor hash functions extractors

--- metext/plugins/extractors/hash.py
+++ metext/plugins/extractors/hash.py
@@ -1,6 +1,7 @@
-from typing import Iterable, List, Union
+from typing import Iterable
 
 from metext.plugin_base import BaseExtractor
+from metext.plugins.extractors import _extract_with_regex
 from metext.utils.regex import (
     RE_MD5,
     RE_SHA1,
@@ -15,99 +16,81 @@ class MD5Extractor(BaseExtractor):
     PLUGIN_NAME = "md5"
 
     @classmethod
-    def run(cls, _input: Union[str, List[str]], **kwargs) -> Iterable[str]:
+    def run(cls, _input: str, **kwargs) -> Iterable[dict]:
         """Extracts strings that conform to the MD5 hash string.
 
         :param _input: String or a list of strings to extract MD5 hash string from
         :param kwargs: Arbitrary keyword arguments
         :return: Generator of MD5 hash strings
         """
-        for part in _input if isinstance(_input, list) else _input.splitlines():
-            if not part:
-                continue
-            yield from iter(RE_MD5.findall(part))
+        yield from _extract_with_regex(_input, RE_MD5, per_line=True)
 
 
 class SHA1Extractor(BaseExtractor):
     PLUGIN_NAME = "sha1"
 
     @classmethod
-    def run(cls, _input: Union[str, List[str]], **kwargs) -> Iterable[str]:
+    def run(cls, _input: str, **kwargs) -> Iterable[dict]:
         """Extracts strings that is in accordance with the SHA-1 hash string.
 
         :param _input: String or a list of strings to extract SHA-1 hash string from
         :param kwargs: Arbitrary keyword arguments
         :return: Generator of SHA-1 hash strings
         """
-        for part in _input if isinstance(_input, list) else _input.splitlines():
-            if not part:
-                continue
-            yield from iter(RE_SHA1.findall(part))
+        yield from _extract_with_regex(_input, RE_SHA1, per_line=True)
 
 
 class SHA224Extractor(BaseExtractor):
     PLUGIN_NAME = "sha224"
 
     @classmethod
-    def run(cls, _input: Union[str, List[str]], **kwargs) -> Iterable[str]:
+    def run(cls, _input: str, **kwargs) -> Iterable[dict]:
         """Extracts strings that is in accordance with the SHA-224 hash string.
 
         :param _input: String or a list of strings to extract SHA-224 hash string from
         :param kwargs: Arbitrary keyword arguments
         :return: Generator of SHA-224 hash strings
         """
-        for part in _input if isinstance(_input, list) else _input.splitlines():
-            if not part:
-                continue
-            yield from iter(RE_SHA224.findall(part))
+        yield from _extract_with_regex(_input, RE_SHA224, per_line=True)
 
 
 class SHA256Extractor(BaseExtractor):
     PLUGIN_NAME = "sha256"
 
     @classmethod
-    def run(cls, _input: Union[str, List[str]], **kwargs) -> Iterable[str]:
+    def run(cls, _input: str, **kwargs) -> Iterable[dict]:
         """Extracts strings that is in accordance with the SHA-256 hash string.
 
         :param _input: String or a list of strings to extract SHA-256 hash string from
         :param kwargs: Arbitrary keyword arguments
         :return: Generator of SHA-256 hash strings
         """
-        for part in _input if isinstance(_input, list) else _input.splitlines():
-            if not part:
-                continue
-            yield from iter(RE_SHA256.findall(part))
+        yield from _extract_with_regex(_input, RE_SHA256, per_line=True)
 
 
 class SHA384Extractor(BaseExtractor):
     PLUGIN_NAME = "sha384"
 
     @classmethod
-    def run(cls, _input: Union[str, List[str]], **kwargs) -> Iterable[str]:
+    def run(cls, _input: str, **kwargs) -> Iterable[dict]:
         """Extracts strings that is in accordance with the SHA-384 hash string.
 
         :param _input: String or a list of strings to extract SHA-384 hash string from
         :param kwargs: Arbitrary keyword arguments
         :return: Generator of SHA-384 hash strings
         """
-        for part in _input if isinstance(_input, list) else _input.splitlines():
-            if not part:
-                continue
-            yield from iter(RE_SHA384.findall(part))
+        yield from _extract_with_regex(_input, RE_SHA384, per_line=True)
 
 
 class SHA512Extractor(BaseExtractor):
     PLUGIN_NAME = "sha512"
 
     @classmethod
-    def run(cls, _input: Union[str, List[str]], **kwargs) -> Iterable[str]:
+    def run(cls, _input: str, **kwargs) -> Iterable[dict]:
         """Extracts strings that is in accordance with the SHA-512 hash string.
 
         :param _input: String or a list of strings to extract SHA-512 hash string from
         :param kwargs: Arbitrary keyword arguments
         :return: Generator of SHA-512 hash strings
         """
-        for part in _input if isinstance(_input, list) else _input.splitlines():
-            if not part:
-                continue
-            yield from iter(RE_SHA512.findall(part))
+        yield from _extract_with_regex(_input, RE_SHA512, per_line=True)

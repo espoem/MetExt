79838ba624efc78fb1980cc6cb6cc3eaebc2238b
Martin Šmíd <martin.smid94@seznam.cz>
2021-04-10 22:52:41 +0200

    Refactor doi extractor

--- metext/plugins/extractors/doi.py
+++ metext/plugins/extractors/doi.py
@@ -1,6 +1,7 @@
 from typing import Iterable, List, Union
 
 from metext.plugin_base import BaseExtractor
+from metext.plugins.extractors import _extract_with_regex
 from metext.plugins.validators.doi import DoiValidator
 from metext.utils.regex import RE_DOI
 
@@ -17,9 +18,6 @@ class DoiExtractor(BaseExtractor):
         :param kwargs: Arbitrary keyword arguments
         :return: Generator with DOI identifiers
         """
-        for part in _input if isinstance(_input, list) else _input.splitlines():
-            if not part:
-                continue
-
-            dois_ = RE_DOI.findall(part)
-            yield from ("doi:" + doi for doi in (dois_) if DoiValidator.run(doi))
+        yield from _extract_with_regex(
+            _input, RE_DOI, validator=DoiValidator.run, per_line=True
+        )

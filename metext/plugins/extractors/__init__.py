5c269e360f48b88f90fac39d326b7319025cd26d
Martin Šmíd <martin.smid94@seznam.cz>
2021-04-11 14:04:48 +0200

    Change extraction result object to contain frequency summary

--- metext/plugins/extractors/__init__.py
+++ metext/plugins/extractors/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 from metext.utils import decode_bytes
 
 
@@ -11,6 +13,32 @@ def _extract_with_regex(
     cached_values=None,
     data_kind=None,
 ):
+    def create_item(value_, position_=None, original_=None, value_kind_=None, **kwargs):
+        res = {"value": value_}
+        if position_ is not None:
+            res.update({"position": position_})
+        if original_ is not None:
+            res.update({"original": original_})
+        if value_kind_ is not None:
+            res.update({"value_kind": value_kind_})
+        if kwargs:
+            res.update(kwargs)
+        return res
+
+    def add_update_item_to_out(item):
+        key_ = json.dumps(item["value"])
+        if key_ not in extracted_values:
+            extracted_values[key_] = item
+        if "frequency" not in extracted_values[key_]:
+            extracted_values[key_]["frequency"] = 0
+        extracted_values[key_]["frequency"] += 1
+        if "positions" not in extracted_values[key_]:
+            extracted_values[key_]["positions"] = []
+        if item.get("position"):
+            extracted_values[key_]["positions"].append(item["position"])
+        if "position" in extracted_values[key_]:
+            del extracted_values[key_]["position"]
+
     if not isinstance(_input, str):
         try:
             _input = decode_bytes(_input)
@@ -18,6 +46,7 @@ def _extract_with_regex(
             yield from ()
 
     cur_pos = 0
+    extracted_values = {}
     for part in _input.splitlines(keepends=True) if per_line else [_input]:
         if preprocess is not None:
             part = preprocess(part)
@@ -28,30 +57,26 @@ def _extract_with_regex(
                 value = postprocess(value)
             orig_value = match.group(0) if match.group(0) != value else None
             if cached_values is not None and value in cached_values:
-                yield _create_res_dict(
-                    value, cur_pos + match.start(0), orig_value, value_kind=data_kind
+                add_update_item_to_out(
+                    create_item(
+                        value,
+                        cur_pos + match.start(0),
+                        orig_value,
+                        value_kind_=data_kind,
+                    )
                 )
                 continue
             if validator is not None and not validator(value):
                 continue
-            yield _create_res_dict(
-                value, cur_pos + match.start(0), orig_value, value_kind=data_kind
+            add_update_item_to_out(
+                create_item(
+                    value, cur_pos + match.start(0), orig_value, value_kind_=data_kind
+                )
             )
+
             if isinstance(cached_values, list):
                 cached_values.append(value)
             if isinstance(cached_values, set):
                 cached_values.add(value)
         cur_pos += len(part)
-
-
-def _create_res_dict(value, position=None, original=None, value_kind=None, **kwargs):
-    res = {"value": value}
-    if position is not None:
-        res.update({"position": position})
-    if original is not None:
-        res.update({"original": original})
-    if value_kind is not None:
-        res.update({"value_kind": value_kind})
-    if kwargs:
-        res.update(kwargs)
-    return res
+    yield from extracted_values.values()

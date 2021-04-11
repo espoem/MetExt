c92567a56f1369e75dc346cf48437c1e15c7452b
Martin Šmíd <martin.smid94@seznam.cz>
2021-04-11 15:35:28 +0200

    Update guid regex

--- metext/utils/regex.py
+++ metext/utils/regex.py
@@ -141,7 +141,7 @@ RE_JSON = regex.compile(
     regex.VERBOSE,
 )
 
-RE_GUID = re.compile(r"\b[0-9a-fA-F]{8}[-]?(?:[0-9a-fA-F]{4}[-]?){3}[0-9a-fA-F]{12}\b")
+RE_GUID = re.compile(r"\b(?<!-)[0-9a-fA-F]{8}[-]?(?:[0-9a-fA-F]{4}[-]?){3}[0-9a-fA-F]{12}(?!-)\b")
 RE_MAC = re.compile(
     r"\b(?<![:-])(?:(?:[0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}|(?:[0-9a-fA-F]{4}\.){2}[0-9a-fA-F]{4})(?![:-])\b"
 )

ee0b2a9053669f5e7e4cf1208e1daaa346253454
Martin Šmíd <martin.smid94@seznam.cz>
2021-04-12 11:50:47 +0200

    Limit extracted guid strings to those with decoded version in 0-5

--- metext/plugins/extractors/guid.py
+++ metext/plugins/extractors/guid.py
@@ -1,12 +1,14 @@
-from typing import Iterable, List, Union
+import re
+import uuid
+from typing import Iterable
 
 from metext.plugin_base import BaseExtractor
 from metext.plugins.extractors import _extract_with_regex
 from metext.utils.regex import RE_GUID
 
 
-class GuidExtractor(BaseExtractor):
-    PLUGIN_NAME = "guid"
+class UuidExtractor(BaseExtractor):
+    PLUGIN_NAME = "uuid"
 
     @classmethod
     def run(cls, _input: str, **kwargs) -> Iterable[dict]:
@@ -16,6 +18,35 @@ class GuidExtractor(BaseExtractor):
         :param kwargs: Arbitrary keyword arguments
         :return: Generator of GUID strings
         """
-        yield from _extract_with_regex(
-            _input, RE_GUID, per_line=True, data_kind=cls.PLUGIN_NAME
-        )
+
+        def validate(uuid, **kwargs):
+            uuid = uuid.replace("-", "")
+            return bool(re.match(r"[0-5]", uuid[12], re.IGNORECASE))
+
+        def get_info(uuid_):
+            try:
+                res = uuid.UUID(uuid_)
+                return {"variant": res.variant, "version": res.version}
+            except:
+                return None
+
+        def normalize(uuid, **kwargs):
+            uuid = uuid.replace(r"-", "").lower()
+            return "{}-{}-{}-{}-{}".format(
+                uuid[:8], uuid[8:12], uuid[12:16], uuid[16:20], uuid[20:]
+            )
+
+        for obj in _extract_with_regex(
+            _input,
+            RE_GUID,
+            per_line=True,
+            data_kind=cls.PLUGIN_NAME,
+            validator=validate,
+            postprocess=normalize,
+        ):
+            info = get_info(obj["value"])
+            if info:
+                if info["version"] is None:
+                    continue
+                obj.update({"info": info})
+            yield obj
